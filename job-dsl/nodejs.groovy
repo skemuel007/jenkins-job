5b9cf3b81c99c7e09271c59061d9b927bda3944b
salvationlloydstanley-kemuel <skemuel008@gmail.com>
2022-11-06 20:06:09 +0100

    Changes updated

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,5 +1,23 @@
-job('NodeJS Docker Jenkins Deploy') {
+job('Nodejs Deployment') {
     scm {
-        
+        git('git://github.com:skemuel007/jenkins-node-docker-demo.git') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('skemuel007@gmail.com')
+        }
     }
+    // how many times the could will build
+    triggers {
+        scm('H/5 * * * *')
+    }
+
+    // the base framework
+    wrappers {
+        nodejs('nodejs')
+        // this is the name of the Nodejs installation in Manage Jenkins -> Configure Tools -> Nodejs Installations
+    }
+
+    steps {
+        shell("npm install")
+    }
+
 }
